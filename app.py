42741323e3a6b42fb8aafe176895e23aa364f39d
Ankur9519050910 <ab7691782@gmail.com>
2026-06-19 11:08:48 +0530

    home_screen_setup

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import streamlit as st
 def main():
+    
     st.header("Hellow Ankur")
     name=st.text_input("Enter Your Nmae")
-    st.button("display my name",type='primary',key='btn1',width='stretch')
-    if st.button("display my name",type='primary',width='content'):
-        print(f'Hello : {name}')
+    col1,col2=st.columns(2,gap='small')
+    with col1:
+       st.button("display my name",type='primary',key='btn1',width='stretch')
+    with col2:
+        if st.button("display my name",type='primary',width='stretch'):
+            print(f'Hello : {name}')
 
-main()
+main() 
+
+
